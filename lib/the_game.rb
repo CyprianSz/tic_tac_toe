a492d72a7175c9800a21f82c7b47b5d7cfa0591c
CyprianS <szwajka.cyprian@gmail.com>
2017-02-20 22:35:59 +0100

    tabs

--- lib/the_game.rb
+++ lib/the_game.rb
@@ -2,79 +2,79 @@ require_relative "board"
 require_relative "player"
 
 class NewGame
-	def initialize 
-		@board = Board.new
-		take_names
-		lets_play
-	end
+  def initialize 
+    @board = Board.new
+    take_names
+    lets_play
+  end
 
-	def take_names
-		puts "\nWelcome to Tic Tac Toe."
-		sleep 2
-		puts "\nGive first player name: "
-		@first_name = gets.chomp
-		@player1 = Player.new(@first_name, '  O ')
-		puts "\nHello #{@first_name} ! Your sign is: O "
-		sleep 2
+  def take_names
+    puts "\nWelcome to Tic Tac Toe."
+    sleep 2
+    puts "\nGive first player name: "
+    @first_name = gets.chomp
+    @player1 = Player.new(@first_name, '  O ')
+    puts "\nHello #{@first_name} ! Your sign is: O "
+    sleep 2
 
-		puts "\nGive second player name: "
-		@second_name = gets.chomp
-		@player2 = Player.new(@second_name, '  X ')
-		puts "\nHello #{@second_name} ! Your sign is: X "
-		sleep 2
-	end
+    puts "\nGive second player name: "
+    @second_name = gets.chomp
+    @player2 = Player.new(@second_name, '  X ')
+    puts "\nHello #{@second_name} ! Your sign is: X "
+    sleep 2
+  end
 
-	def win_or_draw 
-		if ( @board.board[:a1] == @board.board[:a2] && @board.board[:a2] == @board.board[:a3] && @board.board[:a3] != "    " ) ||
-			 ( @board.board[:b1] == @board.board[:b2] && @board.board[:b2] == @board.board[:b3] && @board.board[:b3] != "    " ) ||
-			 ( @board.board[:c1] == @board.board[:c2] && @board.board[:c2] == @board.board[:c3] && @board.board[:c3] != "    " ) ||
-			 ( @board.board[:a1] == @board.board[:b1] && @board.board[:b1] == @board.board[:c1] && @board.board[:c1] != "    " ) ||
-			 ( @board.board[:a2] == @board.board[:b2] && @board.board[:b2] == @board.board[:c2] && @board.board[:c2] != "    " ) ||
-			 ( @board.board[:a3] == @board.board[:b3] && @board.board[:b3] == @board.board[:c3] && @board.board[:c3] != "    " ) ||
-			 ( @board.board[:a1] == @board.board[:b2] && @board.board[:b2] == @board.board[:c3] && @board.board[:c3] != "    " ) ||
-			 ( @board.board[:c1] == @board.board[:b2] && @board.board[:b2] == @board.board[:a3] && @board.board[:a3] != "    " )
-			 puts @board.view_board
-			 puts "\nYou win the game."
-			 puts "Thanks for playing.\n"
-			 true
-		elsif @board.board.each_value.none? { |x| x == "    " }
-			puts @board.view_board
-			puts "\nHere we have a draw."
-			puts "Thanks for playing.\n"
-			true
-		else
-			false
-		end
-	end
+  def win_or_draw 
+    if ( @board.board[:a1] == @board.board[:a2] && @board.board[:a2] == @board.board[:a3] && @board.board[:a3] != "    " ) ||
+       ( @board.board[:b1] == @board.board[:b2] && @board.board[:b2] == @board.board[:b3] && @board.board[:b3] != "    " ) ||
+       ( @board.board[:c1] == @board.board[:c2] && @board.board[:c2] == @board.board[:c3] && @board.board[:c3] != "    " ) ||
+       ( @board.board[:a1] == @board.board[:b1] && @board.board[:b1] == @board.board[:c1] && @board.board[:c1] != "    " ) ||
+       ( @board.board[:a2] == @board.board[:b2] && @board.board[:b2] == @board.board[:c2] && @board.board[:c2] != "    " ) ||
+       ( @board.board[:a3] == @board.board[:b3] && @board.board[:b3] == @board.board[:c3] && @board.board[:c3] != "    " ) ||
+       ( @board.board[:a1] == @board.board[:b2] && @board.board[:b2] == @board.board[:c3] && @board.board[:c3] != "    " ) ||
+       ( @board.board[:c1] == @board.board[:b2] && @board.board[:b2] == @board.board[:a3] && @board.board[:a3] != "    " )
+       puts @board.view_board
+       puts "\nYou win the game."
+       puts "Thanks for playing.\n"
+       true
+    elsif @board.board.each_value.none? { |x| x == "    " }
+      puts @board.view_board
+      puts "\nHere we have a draw."
+      puts "Thanks for playing.\n"
+      true
+    else
+      false
+    end
+  end
 
-	def lets_play
+  def lets_play
 
-		until win_or_draw
-			@board.view_board
-			puts "\n#{@player1.name} - it's your turn. Where to put your sign ?"
-			coordinates = gets.chomp.downcase
-			con1 = coordinates.match /[abc][123]/
-			con2 = @board.board[coordinates.to_sym] == "    "
-			if con1 && con2 
-				@board.board[coordinates.to_sym] = @player1.symbol
-			else
-				puts "\nYou gave wrong coordinates ! You lose your turn."
-			end
+    until win_or_draw
+      @board.view_board
+      puts "\n#{@player1.name} - it's your turn. Where to put your sign ?"
+      coordinates = gets.chomp.downcase
+      con1 = coordinates.match /[abc][123]/
+      con2 = @board.board[coordinates.to_sym] == "    "
+      if con1 && con2 
+        @board.board[coordinates.to_sym] = @player1.symbol
+      else
+        puts "\nYou gave wrong coordinates ! You lose your turn."
+      end
 
-			break if win_or_draw
+      break if win_or_draw
 
-			@board.view_board
-			puts "\n#{@player2.name} - it's your turn. Where to put your sign ?"
-			coordinates = gets.chomp.downcase
-			con1 = coordinates.match /[abc][123]/
-			con2 = @board.board[coordinates.to_sym] == "    "
-			if con1 && con2 
-				@board.board[coordinates.to_sym] = @player2.symbol
-			else
-				puts "\nYou gave wrong coordinates ! You lose your turn."
-			end
-		end
-	end
+      @board.view_board
+      puts "\n#{@player2.name} - it's your turn. Where to put your sign ?"
+      coordinates = gets.chomp.downcase
+      con1 = coordinates.match /[abc][123]/
+      con2 = @board.board[coordinates.to_sym] == "    "
+      if con1 && con2 
+        @board.board[coordinates.to_sym] = @player2.symbol
+      else
+        puts "\nYou gave wrong coordinates ! You lose your turn."
+      end
+    end
+  end
 end
 
-NewGame.new
+#NewGame.new
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,16 +1,22 @@
 require "board"
 
 describe "Board" do
-	before { @board = Board.new }
+  before { @board = Board.new }
 
-	describe "For choosen place on board:" do
-			it "Allows to assign value" do
-				@board.board[:a1] = ' X '
-				expect(@board.board[:a1]).to eql(' X ')
-			end
+  describe "For choosen place on board:" do
+    it "Allows to assign value" do
+      @board.board[:a1] = ' X '
+      expect(@board.board[:a1]).to eql(' X ')
+    end
 
-			it "Reads value" do
-				expect(@board.board[:ha]).to eql(" A ")
-			end
-	end
+    it "Reads value" do
+      expect(@board.board[:ha]).to eql(" A ")
+    end
+  end
+
+  describe "Creates a board" do
+    it "Which is an instance of Hash" do
+      expect(@board.board).to be_an_instance_of(Hash)
+    end
+  end
 end
